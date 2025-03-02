bf146d2095c0e482e357a3c9856d6aaabff2bf0e
Ola Hagerupsen Hansen <96570269+Ola-HH@users.noreply.github.com>
2025-03-02 19:42:07 +0100

    Include frame parameter in definitions to make sure preload works

--- src/components/ConfiguratorPage/ParameterPanel/parameterDefintions.ts
+++ src/components/ConfiguratorPage/ParameterPanel/parameterDefintions.ts
@@ -20,6 +20,21 @@ export interface ParameterDefinition {
 
 export const PARAMETER_DEFINITIONS: ParameterDefinition[] = [
   {
+    id: 'frameType',
+    name: 'Type',
+    type: 'grid',
+    value: '/models/Mango_OSS_Frame.glb',
+    options: [
+      { label: 'OSS', value: '/models/Mango_OSS_Frame.glb', price: 449 }, 
+      { label: 'Moosher', value: '/models/Mango_Moosher_Frame.glb', price: 549 },
+      { label: 'OG', value: '/models/Mango_OG_Frame.glb', price: 649 }, 
+      { label: 'DOG', value: '/models/Mango_DOG_Frame.glb', price: 749 }, 
+    ],
+    category: 'Frame',
+    model: 'Frame',
+    disabled: true,
+  },
+  {
     id: 'frameColor',
     name: 'Color',
     type: 'color',
